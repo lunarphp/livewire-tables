092e5c9be0876661a578e371a052888d86e109e0
Mark Mead <itsmarkmead@gmail.com>
2022-09-01 13:46:19 +0100

    Fix - Asset compiling

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,13 @@
 let mix = require('laravel-mix')
 
 mix
-  .js('resources/js/app.js', 'dist/livewire-tables.js')
-  .postCss('resources/css/app.css', 'dist/livewire-tables.css', [
+  .js('resources/js/app.js', 'dist/livewire-tables/app.js')
+  .setPublicPath('dist')
+  .version()
+
+mix
+  .postCss('resources/css/app.css', 'dist/livewire-tables/app.css', [
     require('tailwindcss'),
   ])
+  .setPublicPath('dist')
+  .version()
