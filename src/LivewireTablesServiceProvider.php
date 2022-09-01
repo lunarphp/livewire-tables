9b66fa60f02e119bbec8399e9abef5717870a6f9
Mark Mead <itsmarkmead@gmail.com>
2022-09-01 14:51:19 +0100

    Fix - Assets path

--- src/LivewireTablesServiceProvider.php
+++ src/LivewireTablesServiceProvider.php
@@ -39,7 +39,7 @@ class LivewireTablesServiceProvider extends ServiceProvider
         Blade::directive('livewireTableAssets', function () {
             $manifest = json_decode(file_get_contents(__DIR__.'/../dist/mix-manifest.json'), true);
 
-            $cssUrl = asset('/vendor/livewire-tables'.$manifest['/livewire-tables/app.css']);
+            $cssUrl = asset('/vendor/getcandy'.$manifest['/livewire-tables/app.css']);
 
             return <<<EOT
                 <link rel="stylesheet" href="{$cssUrl}" />
