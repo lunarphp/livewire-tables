d6ae3d88b708e8d6ddbb8a8edd7d4814843b0a06
Mark Mead <itsmarkmead@gmail.com>
2022-09-02 14:54:28 +0100

    Update - Show active search

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -124,9 +124,21 @@
                                                   wire:click="applySavedSearch({{ $savedSearch['key'] }})"
                                                   class="!lt-border-y-0 !lt-border-r-0 !lt-rounded-l-none focus:!lt-ring-transparent focus:lt-bg-gray-50">
                                     <span @class([
-                                        'text-blue-600' => $this->savedSearch == $savedSearch['key'],
+                                        'lt-inline-flex lt-items-center lt-gap-2',
+                                        'lt-text-blue-600' => $this->savedSearch == $savedSearch['key'],
                                     ])>
                                         {{ $savedSearch['label'] }}
+
+                                        @if ($this->savedSearch == $savedSearch['key'])
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                 viewBox="0 0 20 20"
+                                                 fill="currentColor"
+                                                 class="lt-w-3 lt-h-3">
+                                                <path fill-rule="evenodd"
+                                                      d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                                      clip-rule="evenodd" />
+                                            </svg>
+                                        @endif
                                     </span>
                                 </x-tables::button>
                             </div>
