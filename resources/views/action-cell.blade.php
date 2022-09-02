32a1fa6a53d1ed1a2a2f133b0b9b40f5921d4211
Mark Mead <itsmarkmead@gmail.com>
2022-09-02 13:20:01 +0100

    Update - Table row action dropdown

--- resources/views/action-cell.blade.php
+++ resources/views/action-cell.blade.php
@@ -1,26 +1,20 @@
 <div x-data="{ isActive: false }"
      class="relative">
-    <div
-         class="inline-flex items-stretch bg-white border border-gray-200 rounded-md hover:shadow-sm focus-within:ring focus-within:ring-blue-100">
-        <a href="/edit"
-           class="button button-gray px-2.5 py-2 text-xs !border-0 !rounded-r-none focus:!ring-transparent focus:bg-gray-50">
-            Edit
-        </a>
-
-        <x-tables::button size="xs"
-                          aria-label="Toggle Menu"
-                          x-on:click="isActive = !isActive"
-                          class="!border-y-0 !border-r-0 !rounded-l-none focus:!ring-transparent focus:bg-gray-50">
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 class="w-4 h-4"
-                 viewBox="0 0 20 20"
-                 fill="currentColor">
-                <path fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd" />
-            </svg>
-        </x-tables::button>
-    </div>
+    <x-tables::button size="xs"
+                      aria-label="Toggle Menu"
+                      x-on:click="isActive = !isActive">
+        <span>
+            Actions
+        </span>
+        <svg xmlns="http://www.w3.org/2000/svg"
+             class="w-4 h-4"
+             viewBox="0 0 20 20"
+             fill="currentColor">
+            <path fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd" />
+        </svg>
+    </x-tables::button>
 
     <div x-cloak
          x-transition
--- resources/views/actions/base.blade.php
+++ resources/views/actions/base.blade.php
@@ -2,7 +2,7 @@
     @if ($url)
         <a role="menuitem"
            href="{{ call_user_func($url, $record) }}"
-           class="block px-3 py-2 text-sm font-medium text-gray-600 rounded-md hover:bg-gray-50">
+           class="block px-3 py-2 text-xs font-medium text-gray-600 rounded-md hover:bg-gray-50">
             <span class="capitalize">
                 {{ $label }}
             </span>
